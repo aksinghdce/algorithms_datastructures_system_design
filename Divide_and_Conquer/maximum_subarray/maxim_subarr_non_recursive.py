b96009d292f4ad0ff1be0c6ad9e1540191b22151
Amit Kumar Singh <aksinghdce@gmail.com>
2019-03-09 21:18:31 -0800

    fixed the issue in recursive code

--- Divide_and_Conquer/maximum_subarray/maxim_subarr_non_recursive.py
+++ Divide_and_Conquer/maximum_subarray/maxim_subarr_non_recursive.py
@@ -1,3 +1,4 @@
+#!python
 # Write a recursive function to get a maximum subarray of an array
 # Step 1: Write the program on paper
 # Step 2: Manually validate the accuracy of the program with examples
@@ -21,6 +22,7 @@ def find_maximum_subarray(A:list, low:int, high:int) -> (int, int, int):
     return (left_index, right_index, sum)
 if __name__ == '__main__':
     #l, r, s = find_maximum_subarray([13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7], 0, 15)
-    l, r, s = find_maximum_subarray([-2, -3, 4, -1, -2, 1, 5, -3], 0, 7)
+    #l, r, s = find_maximum_subarray([-2, -3, 4, -1, -2, 1, 5, -3], 0, 7)
+    l, r, s = find_maximum_subarray([2, 5, 1, 7, -21, 30], 0, 5)
     #l, r, s = find_maximum_subarray([-31, -41, 59, 26, -53, 58, 97, -93, -23, 84], 0, 9)
     print("Left_index:",l,"Right_index:",r,"Sum:",s)
