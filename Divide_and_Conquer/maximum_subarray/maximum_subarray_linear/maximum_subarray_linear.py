ccf048d67d7f41fc338fb1c41e908fcf7961899c
Amit Kumar Singh <aksinghdce@gmail.com>
2019-03-15 14:53:12 -0700

    added comments

--- Divide_and_Conquer/maximum_subarray/maximum_subarray_linear/maximum_subarray_linear.py
+++ Divide_and_Conquer/maximum_subarray/maximum_subarray_linear/maximum_subarray_linear.py
@@ -6,13 +6,12 @@ Algorithm:
         Step 3: if sum_k > sum_j, then update li, ui and sum_j
 '''
 def maximum_subarray_linear(A:list):
-    MSSSF = (0,0,0)
-    i:int = 0
-    while i < len(A):
+    MSSSF = (0,0,0) #initialize low, high and sum to 0, 0 and 0 respectively
+    i:int = 0 #index from beginning of the array
+    while i < len(A): # index to end
         l:int = MSSSF[0] #lower_limit_max_sub_arr
         m:int = MSSSF[1] #upper_limit_max_sub_arr
         S:int = MSSSF[2] #sum_max_sub_arr
-        j:int = l #initial condition i=0, j=0
         assert l <= i
         assert m <= i
         #case1 : the current element by itself is greater than the running sum_max_subarray
@@ -23,20 +22,20 @@ def maximum_subarray_linear(A:list):
         #case2: the current elements is contiguous with the current max_subarray
         elif (m == i):
             #extended sum
-            _eS_:int = S + A[i]
+            _eS_:int = S + A[i] #new sum introduces the currently seen element
             #case2.1: check whether new element extends the max subarray
-            k = l
+            k = l # begin from current low and move the low towards i
             _cS_:int = _eS_
             while k <= i:
                 if _cS_ > S:
                     S = _cS_
                     l = k
                     m = i
-                _cS_ = _cS_ - A[k]
+                _cS_ = _cS_ - A[k] # see one less element in the current max subarray contained within l and m
                 k += 1
         elif m < i:#this means maximum subarray can be anywhere in between 1 and i
             p = i# index into subarray A[0, i]
-            while p >= 0:
+            while p >= l: #this looks non-linear because of this inner loop.
                 sum:int = A[p]
                 q:int = p - 1
                 while q>=0:
